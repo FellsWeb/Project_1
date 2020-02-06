533c5af16492d6fd998c4b1c0b5a355813bf0969
Mariana <mariana.diaz@live.com>
2020-02-05 19:09:57 -0500

    link event form user input to firebase, create link on main page to event form, create link to main page on event form

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let configAPI = config;
+firebase.initializeApp(configAPI);
+let database = firebase.database();
+
+
+
 $(document).ready(function() {
     var userIssue = ""
     var userZipCode = ""
@@ -11,3 +17,43 @@ $(document).ready(function() {
         $("#results-dump").append(userIssue + "<br>" + userZipCode)
     })
 });
+
+
+
+
+let eventName = "";
+let socialCause = "";
+let eventTime = 0;
+let eventDate = 0;
+let locationName = "";
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    eventName = $("#eventName-input").val().trim();
+    socialCause = $("#socialcause-input").val().trim();
+    eventTime = $("#time-input").val().trim();
+    eventDate = $("#date-input").val().trim();
+    locationName =$("#location-input").val().trim();
+
+    database.ref().push({
+        eventName: eventName,
+        socialCause: socialCause,
+        eventTime: eventTime,
+        eventDate: eventDate,
+        locationName: locationName,
+
+    });
+
+});
+
+
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+    console.log(childSnapshot.val().eventName);
+    console.log(childSnapshot.val().socialCause);
+    console.log(childSnapshot.val().eventTime);
+    console.log(childSnapshot.val().eventDate);
+    console.log(childSnapshot.val().locationName);
+
+})
+
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyChJ-ZJk65CXkeSo1kCNYznfINdouRFphI",
+    authDomain: "project-1-85a2d.firebaseapp.com",
+    databaseURL: "https://project-1-85a2d.firebaseio.com",
+    projectId: "project-1-85a2d",
+    storageBucket: "project-1-85a2d.appspot.com",
+    messagingSenderId: "486651044397",
+    appId: "1:486651044397:web:97061bbf3de149ddfecea4"
+};

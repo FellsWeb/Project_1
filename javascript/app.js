ab84737c11836a47e8eecafb38a3a978163a2759
Mariana <mariana.diaz@live.com>
2020-02-11 16:08:59 -0500

    changes to current events page, add image

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,16 @@
 (function ($) {
     $(function () {
-
         $('.sidenav').sidenav();
+    });
+})(jQuery);
 
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function () {
+    $('select').formSelect();
+});
 
 let configAPI = config;
 firebase.initializeApp(configAPI);
 let database = firebase.database();
-
-
 
 $(document).ready(function () {
     var userIssue = ""
@@ -22,7 +22,7 @@ $(document).ready(function () {
         userIssue = $("#user-issue-input").val().trim();
         userZipCode = $("#user-zip").val().trim();
 
-        $("#results-dump").append(userIssue + "<br>" + userZipCode)
+        $("#results-dump").append(userIssue + "<br>" + userZipCode);
     })
 });
 
@@ -34,7 +34,9 @@ let userInput = {
     eventTime: 0,
     eventDate: 0,
     locationName: "",
+    address: "",
     hostName: "",
+    guestsAttending: 0,
 }
 
 
@@ -42,61 +44,91 @@ $("#submit").on("click", function (event) {
     event.preventDefault();
     userInput = {
         eventName: $("#eventName-input").val().trim(),
-        socialCause: $("#socialcause-input").val().trim(),
+        socialCause: $('#socialcause-input :selected').text(),
         eventTime: $("#time-input").val().trim(),
         eventDate: $("#date-input").val().trim(),
         locationName: $("#location-input").val().trim(),
+        address: $("#address-input").val().trim(),
         hostName: $("#host-input").val().trim(),
+        guestsAttending: ("#attend-event").val().trim(),
+
     }
 
+    console.log($('#socialcause-input :selected').text())
+
+    //Validate date input
+    var dateEntered = $('#date-input').val();
+    if (moment(dateEntered, 'MM-DD-YYYY').isValid()) {
+        console.log("Date is valid");
+    } else {
+        $(".popup-content, #alert").addClass("active");
+        $("#alert").append("<p>" + "Please use MM-DD-YYYY format" + "<p>");
+    }
 
     database.ref().push({
         userInput
     });
 
     $("#eventName-input").val("");
-    $("#socialcause-input").val("");
-    $("#time-input").val("");
+    $('#socialcause-input :selected').text(""),
+        $("#time-input").val("");
     $("#date-input").val("");
     $("#location-input").val("");
-    $("#host-input").val().trim();
+    $("#address-input").val("");
+    $("#host-input").val("");
 });
 
 
 database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
-
-    let newRow = $("<tr>").append(
-        $("<card-body>").append(
-            $("<td>").append(
+    //display events on table
+    var newRow = $("<tr>").append(
+        $("<td>").attr("id", childSnapshot.key)
+            .append(
                 $("<tr>").text("Event Name : " + childSnapshot.val().userInput.eventName),
                 $("<tr>").text("Hosted by : " + childSnapshot.val().userInput.hostName),
-            ))
+            ));
 
-        /*$("<td>").text(childSnapshot.val(). userInput.hostName),
-        /*$("<td>").text(childSnapshot.val().userInput.socialCause), 
-        $("<td>").text(childSnapshot.val().userInput.eventTime),
-        $("<td>").text(childSnapshot.val().userInput.eventDate), 
-        $("<td>").text(childSnapshot.val().userInput.locationName),*/
-    );
+
+    var eventId = childSnapshot.key;
+
+    var eventInfo = childSnapshot.val().userInput;
 
     $("#eventsTable > tbody").append(newRow);
 
-    $("#eventsTable tbody tr").on("click", function (event) {
-        console.log("clicked2");
-        event.preventDefault();
-        
-    })
+    //display event info in modal when event clicked
+    $("#" + eventId).on("click", function () {
+        $("#event-info p").empty();
+        console.log(eventInfo);
+        console.log(eventId);
+        $(".popup, .popup-content").addClass("active");
+        $("#event-info").append("<p>" + "Event Name: " + childSnapshot.val().userInput.eventName + "<p>");
+        $("#event-info").append("<p>" + "Social Cause: " + childSnapshot.val().userInput.socialCause + "<p>");
+        $("#event-info").append("<p>" + "Time: " + childSnapshot.val().userInput.eventTime + "<p>");
+        $("#event-info").append("<p>" + "Date: " + childSnapshot.val().userInput.eventDate + "<p>");
+        $("#event-info").append("<p>" + "Location: " + childSnapshot.val().userInput.locationName + "<p>");
+        $("#event-info").append("<p>" + "Address: " + childSnapshot.val().userInput.address + "<p>");
+        $("#event-info").append("<p>" + "Hosted By: " + childSnapshot.val().userInput.hostName + "<p>");
+        console.log(childSnapshot.val().userInput.address);
+        console.log(childSnapshot.val().userInput.locationName);
+    });
 
+    //close modal when button clicked
+    $(".close, .popup").on("click", function () {
+        $(".popup, .popup-content").removeClass("active");
+    });
 
-}), function (errorObject) {
+}, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
-}
+
+});
 
 
-
-
-
-
-
+$("#attend-event").on("click", function () {
+    guestsAttending++;
+    database.ref().push({
+        guestsAttending
+    })
+    $("#event-info").append("<p>" + "Number of Guests Attending; " + guestsAttending);
+    console.log(guestsAttending);
+})
 
